6bbdd61f34af10d227a593269d0b3fa6595c18c1
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-03-08 23:26:32 +0700

    recover

--- quiz-pointer/quiz.go
+++ quiz-pointer/quiz.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
 type Gamer struct {
 	Name  string
@@ -11,17 +15,42 @@ func (game *Gamer) AddGame(nama string) {
 	game.Games = append(game.Games, nama)
 }
 
-func main() {
-	gamer := Gamer{Name: "Zelda"}
+func catch() {
+	if r := recover(); r != nil {
+		fmt.Println("error eccured", r)
+	} else {
+		fmt.Println("application running normally")
+	}
+}
 
-	gamer.AddGame("Super Mario")
-	gamer.AddGame("Street Fighter")
-	gamer.AddGame("GTA")
-	gamer.AddGame("Mario Kart")
-	gamer.AddGame("mobile legends")
-	gamer.AddGame("Call of Duty")
-	for _, game := range gamer.Games {
-		fmt.Println(game)
+func validate(input string) (bool, error) {
+	if strings.TrimSpace(input) == "" {
+		return false, errors.New("cannot be empty")
 	}
 
+	return true, nil
+}
+
+func main() {
+	// gamer := Gamer{Name: "Zelda"}
+
+	// gamer.AddGame("Super Mario")
+	// gamer.AddGame("Street Fighter")
+	// gamer.AddGame("GTA")
+	// gamer.AddGame("Mario Kart")
+	// gamer.AddGame("mobile legends")
+	// gamer.AddGame("Call of Duty")
+	// for _, game := range gamer.Games {
+	// 	fmt.Println(game)
+	// }
+	defer catch()
+	var name string
+	fmt.Println("enter your name ")
+	fmt.Scanln(&name)
+
+	if valid, err := validate(name); valid {
+		fmt.Println("hallo ", name)
+	} else {
+		fmt.Println(err.Error())
+	}
 }
